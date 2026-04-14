724761b21c1afcaaff2b61b62b8066ea74d12195
PRATHAM MEHTA <82135692+Sontakkepratham@users.noreply.github.com>
2026-04-14 16:29:43 +0530

    Refactor image upload and prediction logic
    Refactor image processing and inference code with improved error handling and clearer output labels.

--- app.py
+++ app.py
@@ -26,29 +26,41 @@ IMG_SIZE = (224, 224)
 uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-  try:
-    image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    img = image.resize(IMG_SIZE)
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img,axix=0).astype(np.float32)
-    input_name = session.get_inputs()[0].name
-    output = session.run(None, {input_name: img})
-    prediction = output[0][0][0]
-    THRESHOLD = 0.5
-    if prediction > THRESHOLD:
-      label = "DRONE"
-      confidence = prediction
-    else:
-      label = "BIRD"
-      confidence = 1-prediction
-    st.subheader(f"Prediction: {label}")
-    st.write(f"Confidence:{confidence: .2f}")
-    st.progress(int(confidence * 100))
+    try:
+        # Read image
+        image = Image.open(uploaded_file).convert("RGB")
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-except Exception as e:
-    st.error("Error processing image. Try another image.")
-    st.text(str(e))
+        # Preprocess
+        img = image.resize(IMG_SIZE)
+        img = np.array(img) / 255.0
+        img = np.expand_dims(img, axis=0).astype(np.float32)
+
+        # Inference
+        input_name = session.get_inputs()[0].name
+        output = session.run(None, {input_name: img})
+
+        prediction = output[0][0][0]
+
+        # Threshold
+        THRESHOLD = 0.5
+
+        if prediction > THRESHOLD:
+            label = "🚁 Drone"
+            confidence = prediction
+        else:
+            label = "🐦 Bird"
+            confidence = 1 - prediction
+
+        # Output
+        st.subheader(f"Prediction: {label}")
+        st.write(f"Confidence: {confidence:.2f}")
+        st.progress(int(confidence * 100))
+
+    except Exception as e:
+        st.error("Error processing image. Try another image.")
+        st.text(str(e))
+      
 # FOOTER
 st.markdown("---")
 st.caption("Built as part of Labmentix Project | Custom CNN Model")
